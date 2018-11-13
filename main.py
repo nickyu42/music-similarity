3c3cd7e45c5e13664628247037365b5bf8b8d589
Nick Yu <nickyu42@gmail.com>
2018-11-13 15:21:13 +0100

    Added basic signal processing

--- main.py
+++ main.py
@@ -1,40 +1,52 @@
+"""
+Author: Nick Yu
+Date created: 11/11/2018
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io.wavfile
 
+from processing import convert_to_frame
+
 AUDIO_FILE = 'crossing field.wav'
-FRAME_SIZE = 512
+
+# The amount of ms a frame should be
+FRAME_SIZE = 0.025
+
+# The frame overlap
+FRAME_OVERLAP = 0.010
 
 sample_rate, signal = scipy.io.wavfile.read(AUDIO_FILE)
 
-#%% convert to mono audio
+frame_size = int(round(FRAME_SIZE * sample_rate))
+frame_step = int(round(FRAME_OVERLAP * sample_rate))
+
+# convert to mono audio
 signal = (signal[:, 0] + signal[:, 1]) / 2.0
 
 # take first 3s
 signal = signal[:int(3*sample_rate)]
 
 # apply pre-emphasis filter
-pre_emphasis = 0.97
-signal = np.append(signal[0], signal[1:] - pre_emphasis * signal[:-1])
+# pre_emphasis = 0.97
+# signal = np.append(signal[0], signal[1:] - pre_emphasis * signal[:-1])
 
-#%% split into frames
-padding = FRAME_SIZE - (len(signal) % FRAME_SIZE)
-signal = np.append(signal, np.zeros(padding))
-signal = np.array(np.split(signal, FRAME_SIZE))
-
-#%% convert each to frequency domain and apply hamming window
+# convert each to frequency domain and apply hamming window
 # w = lambda n, size: 0.53836 - 0.46164*np.cos(2*np.pi*n / (size - 1))
 
+signal = convert_to_frame(signal, frame_size, frame_step)
+
 # convert to complex numbers
-signal = signal.astype(complex)
+nfft = signal.shape[0] // 2 + 1
+result_signal = np.empty((nfft, signal.shape[1]), dtype=complex)
 
 for i in range(signal.shape[1]):
-    signal[:, i] *= np.hamming(FRAME_SIZE)
-    signal[:, i] = np.fft.fft(signal[:, i])
-    # plt.plot(abs(data[:, i]))
+    frame = signal[:, i] * np.hamming(frame_size)
+    result_signal[:, i] = np.fft.fft(frame)[:nfft]
 
-
-plt.pcolormesh(abs(signal))
+plt.pcolormesh(abs(result_signal))
 plt.show()
+
 
 
---
+++ processing.py
@@ -0,0 +1,45 @@
+"""
+Author: Nick Yu
+Date created: 13/11/2018
+"""
+
+import numpy as np
+import math
+
+
+def convert_to_frame(signal, frame_length, frame_step):
+    """Converts a signal into overlapping frames.
+    If the signal is not a multitude of the frame step, it is zero-padded
+
+    :param signal: Signal to split up
+    :param frame_length: The amount of elements in each frame
+    :param frame_step: The amount of elements to start from
+    :return: two-dimensional numpy.ndarray with frames in the first axis
+    """
+    signal_len = len(signal)
+    frame_count = math.ceil((signal_len - frame_length) / frame_step) + 1
+
+    total_length = (frame_count - 1)*frame_step + frame_length
+    padding = np.zeros((total_length - signal_len,))
+    signal = np.concatenate((signal, padding))
+
+    remaining_frames = [signal[i*frame_step:i*frame_step + frame_length] for i in range(1, frame_count)]
+
+    # concatenate the first frame with the frames at steps
+    signal = np.concatenate((
+        [signal[:frame_length]],
+        remaining_frames
+    )).T
+
+    return signal
+
+
+def create_filter_bank(sample_rate, nbanks, nfft):
+    """Create n Mel filterbanks.
+
+    :param sample_rate: The sampling frequency of the signal
+    :param nbanks: The amount of filterbanks to generate
+    :param nfft: The number of fft coefficients used
+    :return: numpy.ndarray of nfft sized filters
+    """
+    pass
