6f0ee889e61f61fd2653d1597ecfe182f5ed7563
Nick Yu <nickyu42@gmail.com>
2019-07-02 14:08:10 +0200

    Change file structure and add feature_extraction

---
+++ analysis/processing.py
@@ -0,0 +1,127 @@
+"""
+Author: Nick Yu
+Date created: 13/11/2018
+"""
+
+import numpy as np
+import math
+import scipy.io.wavfile
+from scipy.fftpack import dct
+
+
+def convert_to_mfcc(audio_file, frame_size=0.025, frame_overlap=0.01, n_coeff=26, n_ceps=12):
+    """
+    Converts given audio file to mfcc
+    :param audio_file: location of audio file
+    :param frame_size: the amount of ms a frame should be
+    :param frame_overlap: the amount of ms a frame should overlap
+    :param n_coeff: number of fourier coefficients to use
+    :param n_ceps: number of mel ceps to return
+    :return: matrix of mfccs
+    """
+    sample_rate, signal = scipy.io.wavfile.read(audio_file)
+
+    frame_size = int(round(frame_size * sample_rate))
+    frame_step = int(round(frame_overlap * sample_rate))
+
+    # convert to mono audio
+    signal = (signal[:, 0] + signal[:, 1]) / 2.0
+
+    signal = convert_to_frame(signal, frame_size, frame_step)
+
+    # convert to complex numbers
+    nfft = signal.shape[0]
+    nfft_unique = math.floor(nfft / 2 + 1)
+    result_signal = np.empty((nfft_unique, signal.shape[1]), dtype=complex)
+
+    for i in range(signal.shape[1]):
+        frame = signal[:, i] * np.hamming(frame_size)
+        result_signal[:, i] = np.abs(np.fft.fft(frame)[:nfft_unique])
+
+    # compute MFCCs
+    filterbanks = create_filter_bank(sample_rate, n_coeff, nfft)
+
+    filterbanks = np.dot(result_signal.T, filterbanks.T).T
+
+    return dct(np.log10(filterbanks), axis=0)[1:n_ceps + 1]
+
+
+def convert_to_frame(signal, frame_length, frame_step):
+    """Converts a signal into overlapping frames.
+    If the signal is not a multitude of the frame step, it is zero-padded
+
+    :param signal: Signal to split up
+    :param frame_length: The amount of elements in each frame
+    :param frame_step: The amount of elements to start from
+    :return: two-dimensional numpy.ndarray with frames in the first axis
+    """
+    signal_len = len(signal)
+    frame_count = math.ceil((signal_len - frame_length) / frame_step) + 1
+
+    total_length = (frame_count - 1)*frame_step + frame_length
+    padding = np.zeros((total_length - signal_len,))
+    signal = np.concatenate((signal, padding))
+
+    remaining_frames = [signal[i*frame_step:i*frame_step + frame_length] for i in range(1, frame_count)]
+
+    # concatenate the first frame with the frames at steps
+    signal = np.concatenate((
+        [signal[:frame_length]],
+        remaining_frames
+    )).T
+
+    return signal
+
+
+def create_filter_bank(sample_rate, nbanks, nfft, lower_limit=0):
+    """Create n Mel filterbanks.
+
+    :param sample_rate: The sampling frequency of the signal
+    :param nbanks: The amount of filterbanks to generate
+    :param nfft: The number of fft coefficients used
+    :param lower_limit: lower frequency limit, by default 0 Hz
+    :return: numpy.ndarray of nfft sized filters
+    """
+    upper_limit = sample_rate / 2
+
+    mel_freq = np.linspace(convert_mel(lower_limit), convert_mel(upper_limit), num=nbanks+2)
+
+    # convert back to hertz and then map to fft bins
+    # TODO: check why nfft+1 instead of nfft
+    fft_bins = [math.floor((nfft + 1) * inverse_mel(m) / sample_rate) for m in mel_freq]
+
+    filter_banks = np.zeros((nbanks, math.floor(nfft / 2 + 1)))
+
+    for i in range(1, nbanks + 1):
+        # lower, center and upper fft bin values
+        f_m_min, f_m, f_m_plus = fft_bins[i-1:i+2]
+
+        # increasing half
+        for k in range(f_m_min, f_m):
+            filter_banks[i - 1, k] = (k - f_m_min) / (f_m - f_m_min)
+
+        # decreasing half
+        for k in range(f_m, f_m_plus):
+            filter_banks[i - 1, k] = (f_m_plus - k) / (f_m_plus - f_m)
+
+    return filter_banks
+
+
+def pre_emphasis_filter(signal, pre_emphasis=0.97):
+    """Returns the signal with a pre emphasis filter applied.
+
+    :param signal: The signal to apply the filter to
+    :param pre_emphasis: The pre_emphasis alpha value
+    :return: resulting signal
+    """
+    return np.append(signal[0], signal[1:] - pre_emphasis * signal[:-1])
+
+
+def convert_mel(f):
+    """Converts the given frequency to Mel scale"""
+    return 1125 * math.log(1 + f / 700)
+
+
+def inverse_mel(m):
+    """Converts the given Mels back to frequency"""
+    return 700 * (math.exp(m / 1125) - 1)
---
+++ analysis/scraper.py
@@ -0,0 +1,87 @@
+"""
+Author: Nick Yu
+Date created: 1/3/2019
+"""
+import praw
+import json
+import bs4
+
+USER_AGENT = 'AnimeThemes wiki scraper (by /u/nickyu42)'
+
+
+def is_link(a):
+    """Checks if given bs4 object is an <a> tag"""
+    return isinstance(a, bs4.element.Tag) and a.has_attr('href')
+
+
+def parse_row(row):
+    """
+    Parses an html table row where the first column is name
+    and second column is a link
+    :param row: bs4.element object
+    :return: name, link pair
+             e.g. ('OP', 'Crossing field')
+    """
+    cols = row.find_all(True)
+
+    if is_link(cols[1].contents[0]):
+        # name, link
+        return cols[0].string, cols[1].a.get('href')
+
+    return cols[0].string, None
+
+
+def create_praw(cred_file):
+    """Creates a Reddit object with credentials from given file"""
+    with open(cred_file, 'r') as f:
+        credentials = json.load(f)['praw']
+
+    reddit = praw.Reddit(client_id=credentials['client_id'],
+                         client_secret=credentials['client_secret'],
+                         user_agent=USER_AGENT)
+
+    return reddit
+
+
+def get_wiki_links(wiki_page, year):
+    """
+    Extracts all headers and songs per header from a /r/AnimeThemes wiki page
+    :param wiki_page: wikipage object
+    :param year: which year of anime to scrape from the wiki
+    :return: list of all headers and table objects in order
+    """
+    soup = bs4.BeautifulSoup(wiki_page[year].content_html, 'html.parser')
+    wiki = soup.find(class_='md wiki')
+
+    return wiki.find_all(['h3', 'table'])
+
+
+def extract_songs(reddit, year):
+    """
+    Extracts links for all ops/eds within a single wiki page
+    :param reddit: praw instance
+    :param year: which wiki page year to scrape
+    :return: dict with key as anime name and list of song, link pairs
+    """
+    wiki_page = reddit.subreddit('AnimeThemes').wiki
+    links = get_wiki_links(wiki_page, year)
+
+    song_table = {}
+    current_anime = ''
+
+    for l in links:
+        if l.name == 'h3':
+            current_anime = l.string
+
+            # WARNING: duplicate detection is not done
+            # another anime with the same name may break links
+            song_table[current_anime] = []
+
+        else:
+            for row in l.tbody.find_all('tr'):
+                name, link = parse_row(row)
+
+                if link is not None:
+                    song_table[current_anime].append((name, link))
+
+    return song_table
